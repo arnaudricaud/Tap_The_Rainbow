4f9f054477201d4f6b86c3ed47a228c6ee6a5e74
davidmills <david.moulin@telecom-st-etienne.fr>
2017-06-06 17:01:14 +0200

    New note :octocat:

--- Tap_The_Rainbow/playsound.cpp
+++ Tap_The_Rainbow/playsound.cpp
@@ -17,6 +17,22 @@ void PlaySound::playNote(int couleurs){
         break;
     case 5 : QSound::play("/C:/Users/moulin-d/Downloads/MLDHat2.wav");
         break;
+    case 6 : QSound::play("/C:/Users/moulin-d/Downloads/MLDClapLess.wav");
+        break;
+    case 7 : QSound::play("/C:/Users/moulin-d/Downloads/MLDSnareDeeper.wav");
+        break;
+    case 8 : QSound::play("/C:/Users/moulin-d/Downloads/MLDKickKnockLong.wav");
+        break;
+    case 9 : QSound::play("/C:/Users/moulin-d/Downloads/MLDCowbell.wav");
+        break;
+    case 10 : QSound::play("/C:/Users/moulin-d/Downloads/MLDHat2.wav");
+        break;
+    case 11 : QSound::play("/C:/Users/moulin-d/Downloads/MLDKickKnockLong.wav");
+        break;
+    case 12 : QSound::play("/C:/Users/moulin-d/Downloads/MLDCowbell.wav");
+        break;
+    case 13 : QSound::play("/C:/Users/moulin-d/Downloads/MLDHat2.wav");
+        break;
     defautl : qDebug()<<"Something went wrong";
         break;
 }
