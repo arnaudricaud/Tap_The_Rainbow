f39600779e2f29ffbc27f3a51ebbe3670d127b79
Arnaud <arno969@gmail.com>
2017-06-07 19:49:03 +0200

    correction traitements

--- Tap_The_Rainbow/traitement_images.cpp
+++ Tap_The_Rainbow/traitement_images.cpp
@@ -23,9 +23,6 @@ void Traitement_images::reconstruction(Mat calibration, Mat capture){
     Canny(imgNDG,imgNDG,100,100);
     morphologyEx(imgNDG,imgNDG,MORPH_DILATE,kernel1); //Dilatation
     floodFill(imgNDG,cv::Point(50,10),Scalar(255)); //Remplissage
-
-//    floodFill(imgNDG,cv::Point(0,0),Scalar(255));
-//    floodFill(imgNDG,cv::Point(1590,0),Scalar(255));
     bitwise_not(imgNDG,imgNDG); //Complémentaire
 
     // Récupération des RONDS uniquement! (Evite légèrement le pb des ombres)
@@ -42,20 +39,12 @@ void Traitement_images::reconstruction(Mat calibration, Mat capture){
 
     // soustraction
     soustraction=origine-frame; //SOUSTRACTION => Doigt sur les ronds
-    // ouverture
-    //    Mat kernel = getStructuringElement(MORPH_ELLIPSE,Size(50,50));//Nécessaire? NON!
-    //    morphologyEx(soustraction,soustraction,MORPH_OPEN,kernel);
 
     // binarisation
     cvtColor(soustraction,soustraction,CV_RGB2GRAY);
+    threshold(soustraction,soustraction,35,255,CV_THRESH_BINARY); //35
 
-    // /!\ Equal Hist!!! Pas de pb si on ne touche pas la nappe (zone +claire => 255??)
-    equalizeHist(soustraction,soustraction);    //Nécessaire? OUI! (seuil plus haut et évite les problème en cas de pb sombre!)
-    //imshow("zizi",soustraction);
-    threshold(soustraction,soustraction,230,255,CV_THRESH_BINARY); //150 semble ok car equalizeHist!
-    //imshow("zizi2",soustraction);
     //PREND 66ms
-
     // detection de couleur et son
     detectCouleur(origine, soustraction);
 
@@ -77,44 +66,43 @@ void Traitement_images::detectCouleur(Mat image, Mat mask){
             }
         }
     }
-
     //PREND 24 ms
 
 
     //DEBUT TIMER
-
-
     // determination de la couleur
     moyenne=valeur/compteur;
     //imshow("resultat finale",image);
-    QElapsedTimer timer;
-    timer.start();
+    //QElapsedTimer timer;
+    //timer.start();
     float teinte=0;
     teinte=moyenne*360/180;
-    if(teinte>15 && teinte<45){
-        qDebug()<<"orange";
-    }else if(teinte>=45 && teinte<75){
-        qDebug()<<"jaune";
-    }else if(teinte>=75 && teinte<105){
-        qDebug()<<"jaune-vert";
-    }else if(teinte>=105 && teinte<135){
-        qDebug()<<"vert";
-        note.playNote(1);
-    }else if(teinte>=135 && teinte<165){
-        qDebug()<<"bleu-vert";
-    }else if(teinte>=165 && teinte<195){
-        qDebug()<<"cyan";
-    }else if(teinte>=195 && teinte<225){
-        qDebug()<<"bleu moyen";
-    }else if(teinte>=225 && teinte<255){
-        qDebug()<<"bleu ";
-    }else if(teinte>=255 && teinte<285){
-        qDebug()<<"violet";
-    }else if(teinte>=285 && teinte<315){
-        qDebug()<<"magenta";
-    }else if(teinte>=315 && teinte<345){
-        qDebug()<<"rose";
-    }else{qDebug()<<"rouge";
+    if (compteur>1000){
+        if(teinte>15 && teinte<45){
+            qDebug()<<"orange";
+        }else if(teinte>=45 && teinte<75){
+            qDebug()<<"jaune";
+        }else if(teinte>=75 && teinte<105){
+            qDebug()<<"jaune-vert";
+        }else if(teinte>=105 && teinte<135){
+            qDebug()<<"vert";
+            note.playNote(1);
+        }else if(teinte>=135 && teinte<165){
+            qDebug()<<"bleu-vert";
+        }else if(teinte>=165 && teinte<195){
+            qDebug()<<"cyan";
+        }else if(teinte>=195 && teinte<225){
+            qDebug()<<"bleu moyen";
+        }else if(teinte>=225 && teinte<255){
+            qDebug()<<"bleu ";
+        }else if(teinte>=255 && teinte<285){
+            qDebug()<<"violet";
+        }else if(teinte>=285 && teinte<315){
+            qDebug()<<"magenta";
+        }else if(teinte>=315 && teinte<345){
+            qDebug()<<"rose";
+        }else{qDebug()<<"rouge";
+        }
     }
 
     //PRENDS 13 ms
