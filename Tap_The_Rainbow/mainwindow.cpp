a40a9c9fa99a5c81697124f074c4e869cfb7cc39
Aurelien-Valeille <aurelien.valeille@sfr.fr>
2017-06-07 12:23:44 +0200

    INTERFACE V5 turbo diesel :car:

--- Tap_The_Rainbow/mainwindow.cpp
+++ Tap_The_Rainbow/mainwindow.cpp
@@ -37,7 +37,10 @@ MainWindow::MainWindow(QWidget *parent) :
     height=height/2;
     cam->set(CV_CAP_PROP_FRAME_WIDTH, width);
     cam->set(CV_CAP_PROP_FRAME_HEIGHT, width);
+
     connect(timer, SIGNAL(timeout()), this, SLOT(update()));
+    connect(soloT, SIGNAL(timeout()), this, SLOT(modeSolo()));
+    connect(multiT, SIGNAL(timeout()), this, SLOT(modeMulti()));
 
    /* calibration = imread("C:/Users/Arnaud/Pictures/Camera Roll/IMG 2.0/calibration1.jpg");
     capture = imread("C:/Users/Arnaud/Pictures/Camera Roll/IMG 2.0/rouge3.jpg");
@@ -53,6 +56,64 @@ MainWindow::~MainWindow()
     delete ui;
     delete cam;
 }
+void MainWindow::modeSolo(){
+    cvtColor(image,image,CV_BGR2RGB);
+    if(calibrationOk){
+       TI.reconstruction(calibrationImg,image);
+    }
+}
+
+void MainWindow::modeMulti(){
+    //decoupageImage();
+    Point maxLoc;
+    maxLoc.x=centreImg.x;
+    maxLoc.y=centreImg.y;
+    cvtColor(image,image,CV_BGR2RGB);
+    // HAUT GAUCHE
+    Img1T =image;
+    Img1T =Img1T.colRange(0,maxLoc.x);
+    Img1T=Img1T.rowRange(0,maxLoc.y);
+
+    // HAUT DROIT
+    Img2T =image;
+    Img2T=Img2T.colRange(maxLoc.x,width);
+    Img2T=Img2T.rowRange(0,maxLoc.y);
+
+    // BAS GAUCHE
+    Img3T =image;
+    Img3T=Img3T.colRange(0,maxLoc.x);
+    Img3T=Img3T.rowRange(maxLoc.y,height);
+
+    // BAS DROIT
+    Img4T =image;
+    Img4T=Img4T.colRange(maxLoc.x,width);
+    Img4T=Img4T.rowRange(maxLoc.y,height);
+
+    //Test d'affichage
+   /* namedWindow("Haut GAUCHE",1);
+    imshow("Haut GAUCHE", Img1T);
+    namedWindow("HAUT DROIT",1);
+    imshow("HAUT DROIT", Img2T);
+    namedWindow("BAS GAUCHE",1);
+    imshow("BAS GAUCHE", Img3T);
+    namedWindow("BAS DROIT",1);
+    imshow("BAS DROIT", Img4T);*/
+    //fin test
+
+    // TRAITEMENT si l'instrument est selectionné
+    if(instrument1){
+        TI.reconstruction(Img1,Img1T);
+    }
+    if(instrument2){
+        TI.reconstruction(Img2,Img2T);
+    }
+    if(instrument3){
+        TI.reconstruction(Img3,Img3T);
+    }
+    if(instrument4){
+        TI.reconstruction(Img4,Img4T);
+    }
+}
 void MainWindow::update(){
     if (cam->isOpened()) {
         if (cam->read(image)) {   // Capture a frame
@@ -66,12 +127,6 @@ void MainWindow::update(){
             ui->camFrame->setPixmap(QPixmap::fromImage(img));
             // Resize the label to fit the image
             ui->camFrame->resize(ui->camFrame->pixmap()->size());
-            //TRAITEMENT IMAGE
-            if(calibrationOk){
-               TI.reconstruction(calibrationImg,image);
-            } else {
-                calibrationImg = image;
-            }
         }
     }
     //TESTS
@@ -83,7 +138,7 @@ void MainWindow::update(){
 
 void MainWindow::on_pushButton_clicked()
 {
-    if (ui->pushButton->text()=="Reset Calibration"){
+    if (ui->pushButton->text()=="Reset Calibration"){ //RESET CALIBRATION
      ui->checkAuto->setChecked(false);
      ui->checkAuto->setEnabled(true);
      ui->checkBox->setEnabled(true);
@@ -93,7 +148,9 @@ void MainWindow::on_pushButton_clicked()
      ui->instrument3->setEnabled(false);
      ui->instrument4->setEnabled(false);
      ui->pushButton->setText("Valider");
-    }else{
+     multiT->stop();
+     play=false;
+    }else{ //VALIDER
      ui->checkAuto->setEnabled(false);
      ui->checkBox->setEnabled(false);
      ui->instrument1->setEnabled(true);
@@ -105,6 +162,8 @@ void MainWindow::on_pushButton_clicked()
      ui->instrument3->setChecked(false);
      ui->instrument4->setChecked(false);
      ui->pushButton->setText("Reset Calibration");
+     multiT->start(1000);
+     play=true;
     }
  }
 
@@ -164,9 +223,10 @@ void  MainWindow::mousePressEvent(QMouseEvent *event) {
      key=true;
      PositionX=&lPositionX;
      PositionY=&lPositionY;
-     *PositionX=*PositionX-95;
+     *PositionX=*PositionX-100;
      *PositionY=*PositionY-20;
-
+     centreImg.x=*PositionX;
+     centreImg.y=*PositionY;
     if(*PositionX>0 && *PositionX<320){
         if(*PositionY>0 && *PositionY<240){
             decoupageImage();
@@ -181,15 +241,6 @@ void MainWindow::on_checkAuto_clicked()
     ui->pushButton->setEnabled(true);
 }
 
-void MainWindow::on_imgCalib_clicked()
-{
-    if (ui->imgCalib->isChecked()){
-        calibrationOk = true;
-    } else{
-        calibrationOk = false;
-    }
-
-}
 
 void MainWindow::decoupageImage(){
    if (multi){
@@ -197,6 +248,9 @@ void MainWindow::decoupageImage(){
     maxLoc.x=*PositionX;
     maxLoc.y=*PositionY;
     cvtColor(image,image,CV_BGR2RGB);
+    if (play){
+
+    }  else{
     // HAUT GAUCHE
     Img1 =image;
     Img1 =Img1.colRange(0,maxLoc.x);
@@ -216,7 +270,7 @@ void MainWindow::decoupageImage(){
     Img4 =image;
     Img4=Img4.colRange(maxLoc.x,width);
     Img4=Img4.rowRange(maxLoc.y,height);
-
+    }
     if (imagette && manu && key){
      namedWindow("Haut GAUCHE",1);
      imshow("Haut GAUCHE", Img1);
@@ -238,6 +292,7 @@ void MainWindow::on_butonMulti_clicked()
     ui->butonJoueur->setEnabled(true);
     ui->butonMulti->setEnabled(false);
     multi=true;
+    soloT->stop();
 }
 
 void MainWindow::on_butonJoueur_clicked()
@@ -247,6 +302,7 @@ void MainWindow::on_butonJoueur_clicked()
     ui->butonJoueur->setEnabled(false);
     ui->butonMulti->setEnabled(true);
     multi=false;
+    multiT->stop();
 }
 
 void MainWindow::on_checkBox_clicked()
@@ -271,5 +327,50 @@ void MainWindow::on_checkBox_clicked()
 
 void MainWindow::on_instrument1_clicked()
 {
+    if (ui->instrument1->isChecked()){
+        instrument1=true;
+    }else{
+        instrument1=false;
+    }
+}
 
+void MainWindow::on_instrument3_clicked()
+{
+    if (ui->instrument3->isChecked()){
+        instrument3=true;
+    }else{
+        instrument3=false;
+    }
+}
+
+void MainWindow::on_instrument2_clicked()
+{
+    if (ui->instrument2->isChecked()){
+        instrument2=true;
+    }else{
+        instrument2=false;
+    }
+}
+
+void MainWindow::on_instrument4_clicked()
+{
+    if (ui->instrument4->isChecked()){
+        instrument4=true;
+    }else{
+        instrument4=false;
+    }
+}
+
+void MainWindow::on_calibration_clicked()
+{
+    if(ui->calibration->text()=="Calibration"){
+        calibrationImg = image;
+        calibrationOk=true;
+        ui->calibration->setText("Refaire calibration");
+        soloT->start(1000);
+    }else{
+        calibrationOk=false;
+        ui->calibration->setText("Calibration");
+        soloT->stop();
+    }
 }
--- Tap_The_Rainbow/mainwindow.h
+++ Tap_The_Rainbow/mainwindow.h
@@ -26,22 +26,26 @@ public:
     void decoupageImage();
 private slots:
     void update();
+    void modeSolo();
+    void modeMulti();
     void on_pushButton_clicked();
     void on_checkAuto_clicked();
-    void on_imgCalib_clicked();
-
     void on_butonMulti_clicked();
-
     void on_butonJoueur_clicked();
-
     void on_checkBox_clicked();
-
     void on_instrument1_clicked();
+    void on_instrument3_clicked();
+    void on_instrument2_clicked();
+    void on_instrument4_clicked();
+
+    void on_calibration_clicked();
 
 private:
     Ui::MainWindow *ui;
     VideoCapture *cam;
     QTimer *timer = new QTimer(this);
+    QTimer *soloT = new QTimer(this);
+    QTimer *multiT = new QTimer(this);
     int *PositionX=0;
     int *PositionY=0;
 
@@ -49,21 +53,31 @@ private:
     bool multi = false; //multijoueurs ou mode solo
     bool imagette=false; // on souhaite voir les imagettes
     bool key=false; //valide au moins un clic de l'utilisateur
+    bool play=false;
     //instruments activés
     bool instrument1 = false;
     bool instrument2 = false;
     bool instrument3 = false;
     bool instrument4 = false;
+
     Traitement_images TI;
     bool calibrationOk;
     Mat calibrationImg;
     Mat calibration;
     Mat capture;
+    //image affichée
     Mat image;
+    //imagettes de référence
     Mat Img1;
     Mat Img2;
     Mat Img3;
     Mat Img4;
+    // imagettes pour traitement
+    Mat Img1T;
+    Mat Img2T;
+    Mat Img3T;
+    Mat Img4T;
+
     int width;
     int height;
     Point centreImg;
